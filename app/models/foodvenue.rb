438cd109e843c35637d29414d1a5e221cb42d5e9
Raghu Betina <raghu@firstdraft.com>
2017-01-04 23:44:30 +0000

    Direct association between foodvenues and verybests

--- app/models/foodvenue.rb
+++ app/models/foodvenue.rb
@@ -1,6 +1,9 @@
 class Foodvenue < ApplicationRecord
   # Direct associations
 
+  has_many   :verybests,
+             :dependent => :destroy
+
   belongs_to :venue
 
   belongs_to :food
--- app/models/verybest.rb
+++ app/models/verybest.rb
@@ -1,6 +1,8 @@
 class Verybest < ApplicationRecord
   # Direct associations
 
+  belongs_to :foodvenue
+
   # Indirect associations
 
   # Validations
